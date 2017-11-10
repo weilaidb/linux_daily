22a971e9392f5021fa29664b980c09e7a5b6ba1e
weiwei <wxjlmr@126.com>
2017-11-10 23:14:47 +0800

    update binder_alloc.c

--- binder_alloc.c
+++ binder_alloc.c
@@ -174,6 +174,145 @@ struct binder_buffer * binder_alloc_prepare_to_free(struct binder_alloc *alloc,
 }
 
 
+static int binder_update_page_range(struct binder_alloc *alloc, int allocate,
+							void *start, void *end,
+							struct vm_area_struct *vma)
+{
+	void *page_addr;
+	unsigned long user_page_addr;
+	struct binder_lru_page *page;
+	struct mm_struct *mm = NULL;
+	bool need_mm = false;
+	
+	binder_alloc_debug(BINDER_DEBUG_BUFFER_ALLOC,
+		"%d:%s pages %pK-%pK\n", alloc->pid,
+		alllcate ? "allocate" : "free", start, end);
+	if(end <= start)
+		return 0;
+	
+	trace_binder_update_page_range(alloc, allocate, start, end);
+	
+	if(allocate == 0)
+		goto free_range;
+	
+	for(page_addr = start; page_addr < end; page_addr += PAGE_SIZE) {
+		page = &alloc->pages[(page_addr - alloc->buffer)/ PAGE_SIZE];
+		if(!page->page_ptr) {
+			need_mm = true;
+			break;
+		}
+	}
+	if(!vma && need_mm)
+		mm = get_task_mm(alloc->tsk);
+	
+	if(mm) {
+		down_write(&mm->mmap_sem);
+		vma = alloc->vma;
+		if(vma && mm != alloc->vma_vm_mm) {
+			pr_err("%d:vma mm and task mm mismatch\n",
+				alloc->pid);
+			vma = NULL;
+		}
+	}
+	
+	if(!vma && need_mm) {
+		pr_err("%d: binder_alloc_buf failed to map pages in userspace, no vma\n",
+			alloc->pid);
+		goto err_no_vma;
+	}
+	
+	for(page_addr = start; page_addr < end; page_addr += PAGE_SIZE) {
+		int ret;
+		bool on_lru;
+		size_t index;
+		
+		index = (page_addr - alloc->buffer) / PAGE_SIZE;
+		page = &alloc->pages[index];
+		
+		if(page->page_ptr) {
+			trace_binder_alloc_lru_start(alloc, index);
+			
+			on_lru = list_lru_del(&binder_alloc_lru, &page->lru);
+			WARN_ON(!on_lru);
+			
+			trace_binder_alloc_lru_end(alloc, index);
+			continue;
+		}
+		if(WARN_ON(!vma))
+			goto err_page_ptr_cleared;
+		
+		trace_binder_alloc_page_start(alloc, index);
+		page->page_ptr = alloc_page(GFP_KERNEL |	
+					__GFP_HIGHMEM |
+					__GFP_ZERO);
+		if(!page->page_ptr) {
+			pr_err("%d:binder_alloc_buf failed for page  at %pK\n",
+				alloc->pid, page_addr);
+			goto err_alloc_page_failed;
+		}
+		page->alloc = alloc;
+		INIT_LIST_HEAD(&page->lru);
+		
+		ret = map_kernel_range_noflush((unsigned long)page_addr,
+					PAGE_SIZE, PAGE_KERNEL,
+					&page->page_ptr);
+		flush_cache_vmap((unsigned long)page_addr,
+				(unsigned long)page_addr + PAGE_SIZE);
+		if(ret != 1) {
+			pr_err("%d: binder_alloc_buf failed to map page at %pK in kernel\n",
+				alloc->pid, page_addr);
+			goto  err_map_kernel_failed;
+		}
+		
+		user_page_addr = 
+			(uintptr_t)page_addr + alloc->user_buffer_offset;
+		ret = vm_insert_page(vma, user_page_addr, page[0].page_ptr);
+		if(ret) {
+			pr_err("%d: binder_alloc_buf failed to map page at %lx in userspace\n",
+				alloc->pid, user_page_addr);
+			goto err_vm_insert_page_failed;
+		}
+		trace_binder_alloc_page_end(alloc, index);
+		/** vm_insert_page does not seem to increment the refcount */
+		
+	}
+	if(mm){
+		up_write(&mm->mmap_sem);
+		mmput(mm);
+	}
+	return 0;
+free_range:
+	for(page_addr = end - PAGE_SIZE; page_addr >= start;
+			page_addr -= PAGE_SIZE){
+		bool ret;
+		size_t index;
+		
+		index = (page_addr - alloc->buffer)/ PAGE_SIZE;
+		page = &alloc->pages[index];
+		
+		trace_binder_free_lru_start(alloc, index);
+		
+		ret = list_lru_add(&binder_alloc_lru, &page->lru);
+		WARN_ON(!ret);
+		
+		trace_binder_free_lru_end(alloc, index);
+		continue;
+err_vm_insert_page_failed:
+		unmap_kernel_range((unsigned long)page_ptr, PAGE_SIZE);
+err_map_kernel_failed:
+		__free_page(page->page_ptr);
+		page->page_ptr = NULL;
+err_alloc_page_failed:
+err_page_ptr_cleared:
+		
+	}
+err_no_vma:
+	if(mm) {
+		up_write(&mm->mmap_sem);
+		mmput(mm);
+	}
+	return vma ? -ENOMEM : -ESRCH;
+}
 
 
 
