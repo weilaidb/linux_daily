e0037b044f485555f8914206be8ac4ba14979f0b
weiwei <wxjlmr@126.com>
2017-11-20 21:55:01 +0800

    update do_mounts.c

--- do_mounts.c
+++ do_mounts.c
@@ -489,6 +489,179 @@ static int __init mount_nfs_root(void)
 }
 #endif
 
+#if defined(CONFIG_BLK_DEV_RAM) || defined(CONFIG_BLK_DEV_FD)
+void __init change_floppy(char *fmt, ...)
+{
+	struct termios termios;
+	char buf[80];
+	char c;
+	int fd;
+	va_list args;
+	va_start(args, fmt);
+	vsprintf(buf, fmt, args);
+	va_end(args);
+	fd = sys_open("/dev/root", O_RDWR | O_NDELAY, 0);
+	if(fd>= 0) {
+		sys_ioctl(fd, FDEJECT, 0);
+		sys_close(fd);
+	}
+	printk(KERN_NOTICE "VFS: Insert %s and press ENTER\n", buf);
+	fd = sys_open("/dev/console", O_RDWR, 0);
+	if(fd >= 0) {
+		sys_ioctl(fd, TCGETS, (long)&termios);
+		termios.c_lflag &= ~ICANON;
+		sys_ioctl(fd, TCSETSF, (long)&termios);
+		sys_read(fd, &c, 1);
+		termios.c_lflag |= ICANON;
+		sys_ioctl(fd, TCSETSF, (long)&termios);
+		sys_close(fd);
+	}
+	
+	
+	
+}
+
+#endif
+
+
+void __init mount_root(void)
+{
+#ifdef CONFIG_ROOT_NFS
+	if(ROOT_DEV == Root_NFS){
+		if(mount_nfs_root())
+			return;
+		printk(KERN_ERR "VFS: Unable to mount root fs va NFSROOT_RETRY_MAX, trying floopy.\n");
+		ROOT_DEV = Root_FD0;
+	}
+
+#endif
+
+#ifdef CONFIG_BLK_DEV_FD
+	if(MAJOR(ROOT_DEV) == FLOPPY_MAJOR) {
+		/** rd_doload is 2 for a dual initrd/ ramload setup **/
+		if(rd_doload == 2) {
+			if(rd_load_disk(1)) {
+				ROOT_DEV = Root_Ram1;
+				root_device_name = NULL;
+			}
+		} else 
+			change_floppy("root floppy");
+	}
+#endif
+
+#ifdef CONFIG_BLOCK
+	{
+		int err = create_dev("/dev/root", ROOT_DEV);
+		
+		if(err < 0)
+			pr_emerg("Failed to create /dev/root: %d\n", err);
+		mount_block_root("/dev/root", root_mountflags);
+	}
+#endif
+	
+
+}
+
+/**
+** Prepare the namespace - decide what/ where to mount, load ramdisks, etc
+**/
+void __init prepare_namespace(void)
+{
+	int is_floppy;
+	
+	if(root_delay) {
+		printk(KERN_INFO "Waiting %d sec before mounting root device ...\n",
+			root_delay);
+		ssleep(root_delay);
+	}
+	/**
+	** wait for the known devices to complete their probing
+	**
+	** Note: this is a potential source of long boot delays.
+	** For exmaple, it is not atypical to wait 5 seconds here 
+	** for the touchpad of a laptop to initialize.
+	**/
+	wait_for_device_probe();
+	
+	md_run_setup();
+	
+	if(saved_root_name[0]) {
+		root_device_name = saved_root_name;
+		if(!strncmp(root_device_name,"mtd", 3) ||
+			!strncmp(root_device_name, "ubi", 3)) {
+			mount_block_root(root_device_name, root_mountflags);
+			goto out;
+		}
+		ROOT_DEV = name_to_dev_t(root_device_name);
+		if(strncmp(root_device_name, "/dev/", 5) == 0)
+			root_device_name += 5;
+	}
+	if(initrd_load())
+		goto out;
+	/** wait for  any asynchronous scanning to complete **/
+	if((ROOT_DEV == 0) && root_wait) {
+		printk(KERN_INFO "Waiting for root device %s...\n",
+			saved_root_name);
+		while(driver_probe_done() != 0 ||
+			(ROOT_DEV = name_to_dev_t(saved_root_name)) == 0)
+			msleep(5);
+		async_synchronize_full();
+	}
+	
+	is_floppy = MAJOR(ROOT_DEV) == FLOPPY_MAJOR;
+	
+	if(is_floppy && rd_doload && rd_load_disk(0))
+		ROOT_DEV = Root_RAM0;
+	
+	mount_root();
+out:
+	devtmpfs_mount("dev");
+	sys_mount(".", "/", NULL, MS_MOVE, NULL);
+	sys_chroot(".");
+	
+}
+
+static bool is_tmpfs;
+static struct dentry * rootfs_mount(struct file_system_type *fs_type,
+	int flags, const char *dev_name, void *data)
+{
+	static unsigned long once;
+	void *fill = ramfs_fill_super;
+	
+	if(test_and_set_bit(0, &once))
+		return ERR_PTR(-ENODEV);
+	if(IS_ENABLED(CONFIG_TMPFS) && is_tmpfs)
+		fill = shmem_fill_super;
+	
+	return mount_nodev(fs_type, flags, data, fill);
+	
+}
+
+static struct file_system_type rootfs_fs_type = {
+	.name = "rootfs",
+	.mount = rootfs_mount,
+	.kill_sb = kill_litter_super,
+};
+
+int __init init_roofs(void)
+{
+	int err = register_filesystem(&rootfs_fs_type);
+	
+	if(err)
+		return err;
+	if(IS_ENABLED(CONFIG_TMPFS) && !saved_root_name[0] &&
+		(!root_fs_names || strstr(root_fs_names, "tmpfs"))) {
+		err = shmem_init();
+		is_tmpfs = true;
+	} else {
+		err = init_ramfs_fs();
+	}
+	
+	if(err)
+		unregister_filesystem(&rootfs_fs_type);
+	
+	return err;
+}
 
 
 
