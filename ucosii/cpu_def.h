e61d4052933b965db88fa0e2223721e8a5c258dc
weiwei <wxjlmr@126.com>
2017-11-27 20:16:23 +0800

    update do_mounts_md.c

---
+++ ucosii/cpu_def.h
@@ -0,0 +1,81 @@
+/**
+*************************************************************************************
+*                                       MODULE
+*Note(s): (1) This CPU definition header file is protected from multiple pre-processer inclusion
+**             through use of the CPU definition module present pre-processer macro definition.
+*************************************************************************************
+**/
+
+#ifndef CPU_DEF_MODULE_PRESENT
+#define CPU_DEF_MODULE_PRESENT
+
+/*
+******************************************************************
+*                      CORE CPU MODULE VERSION NUMBER
+**Note(s):(1)(a) The core CPU module software version is denoted as follows:
+*
+*  Vx.yy.zz
+* where 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
